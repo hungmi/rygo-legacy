0edb2726acb8c7f48177eba595f118284247e5b4
hungmi <hungmihungmi@gmail.com>
2018-04-01 19:48:54 +0800

    remove not used cloth.images_or_default

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -3,9 +3,4 @@ class Cloth < ApplicationRecord
 	has_many :order_items, dependent: :destroy
 	has_many_attached :images
 	attr_accessor :remove_images
-
-
-	def images_or_default
-		images.attached? ? images : "http://via.placeholder.com/660x1258/333.jpg"
-	end
 end
