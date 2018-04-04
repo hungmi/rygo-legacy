fc54ea339bca3fe66de560d2ad2050fbf07406ea
hungmi <hungmihungmi@gmail.com>
2018-04-04 10:19:29 +0800

    新增部分翻譯

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -2,5 +2,8 @@ class Cloth < ApplicationRecord
 	belongs_to :supplier, class_name: "User"
 	has_many :order_items, dependent: :destroy
 	has_many_attached :images
+
+	validates :code, presence: true, uniqueness: { allow_blank: true }
+
 	attr_accessor :remove_images
 end
